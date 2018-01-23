d6ffc83395545e509c89e30f1e3cdf7c761a5315
Viccy Grace <viccy.c.grace@gmail.com>
2018-01-23 00:09:35 +0000

    Adding print_uppercase.js

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,15 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printLowerCase (html) {
+
+  /* Write your code here! */
+
+}
+
+getHTML(requestOptions, printLowerCase);
+
---
+++ callbacks/print_reverse.js
@@ -0,0 +1,15 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printReverse (html) {
+
+  /* Write your code here! */
+
+}
+
+getHTML(requestOptions, printReverse);
+
