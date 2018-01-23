694633b7f42344b195e297e0b8b8730496861aa3
Viccy Grace <viccy.c.grace@gmail.com>
2018-01-23 00:21:32 +0000

    changed path for print_lowercase.js and created print_reverse.js

--- callbacks/print_lowercase.js
+++ callbacks/print_lowercase.js
@@ -2,7 +2,7 @@ var getHTML = require('../http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step6/uppercase.html'
+  path: '/http-examples/step6/lowercase.html'
 };
 
 function printLowerCase (html) {
--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -2,13 +2,11 @@ var getHTML = require('../http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step6/uppercase.html'
+  path: '/http-examples/step6/reverse.html'
 };
 
 function printReverse (html) {
-
-  /* Write your code here! */
-
+  console.log(html.split("").reverse().join(""));
 }
 
 getHTML(requestOptions, printReverse);
